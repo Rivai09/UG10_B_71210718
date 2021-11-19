9830f8a8f65d533b11bce48447d38e47b59a537e
Rivai09 <93333522+Rivai09@users.noreply.github.com>
2021-11-19 11:08:45 +0700

    2_B_71210718.py

--- 2_B_71210718.py
+++ 2_B_71210718.py
@@ -4,13 +4,13 @@ RSI = input("Masukkan besar RSI: ")
 MACD = input("Masukkan kondisi MACD: ")
 
 #OUTPUT
-if RSI == "100" :
+if RSI == "100" and MACD == "death-cross" :
     print ("RSI lebih besar sama dengan 70 dan MACD",MACD, "saatnya jual")
-elif RSI == "25":
+elif RSI == "25" and MACD == "golden-cross":
     print ("RSI kurang dari sama dengan 30 dan MACD", MACD, "saatnya beli")
-elif RSI =="100":
+elif RSI =="100" and MACD == "golden-cross":
     print ("RSI lebih dari sama dengan 70 dan MACD", MACD, "tunggu sampai MACD Death-cross")
-elif RSI =="20":
+elif RSI =="20" and MACD == "death-cross":
     print ("RSI kurang dari sama dengan 30 dan MACD", MACD, "tunggu sampai MACD Golden-cross")
-elif RSI == "55":
+elif RSI == "55" and MACD == "golden-cross":
     print ("RSI berada di area 30-70, bukan saatnya membeli atupun menjual")
